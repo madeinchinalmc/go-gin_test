89e0fc64b308c9f802a6495c82483d74d10d4ec5
madeinchinalmc <313801700@qq.com>
2021-05-30 21:19:30 +0800

    segment implementation

--- ConcurrentMap/segment.go
+++ ConcurrentMap/segment.go
@@ -1,6 +1,10 @@
 package concurrentMap
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
 type Segment interface {
 	Put(p Pair) (bool, error)
@@ -37,21 +41,60 @@ func newSegment(bucketNumber int, pairRedistributor PairRedistributor) Segment {
 	}
 }
 
-func (*segment) Put(p Pair) (bool, error) {
-	return true, nil
+func (s *segment) Put(p Pair) (bool, error) {
+	s.lock.Lock()
+	b := s.buckets[int(p.Hash()%uint64(s.bucketsLen))]
+	ok, err := b.Put(p, nil)
+	if ok {
+		newTotal := atomic.AddUint64(&s.pairTotal, 1)
+		s.redistribute(newTotal, b.Size())
+	}
+	s.lock.Unlock()
+	return ok, err
 }
 
-func (*segment) Get(key string) Pair {
+func (s *segment) Get(key string) Pair {
+	return s.GetWithHash(key, hash(key))
+}
+
+func (s *segment) GetWithHash(key string, keyHash uint64) Pair {
+	s.lock.Lock()
+	b := s.buckets[int(keyHash%uint64(s.bucketsLen))]
+	s.lock.Unlock()
+	return b.Get(key)
+}
+
+func (s *segment) Delete(key string) bool {
+	s.lock.Lock()
+	b := s.buckets[int(hash(key)%uint64(s.bucketsLen))]
+	ok := b.Delete(key, nil)
+	if ok {
+		newTotal := atomic.AddUint64(&s.pairTotal, ^uint64(0))
+		s.redistribute(newTotal, b.Size())
+	}
+	s.lock.Unlock()
+	return ok
+}
+func (s *segment) Size() uint64 {
+	return atomic.LoadUint64(&s.pairTotal)
+}
+
+func (s *segment) redistribute(pairTotal uint64, bucketSize uint64) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			if pErr, ok := p.(error); ok {
+				err = newPairRedistributorError(pErr.Error())
+			} else {
+				err = newPairRedistributorError(fmt.Sprintf("%s", p))
+			}
+		}
+	}()
+	s.pairRedistributor.UpdateThreshold(pairTotal, s.bucketsLen)
+	bucketStatus := s.pairRedistributor.CheckBucketStatus(pairTotal, bucketSize)
+	newBuckets, changed := s.pairRedistributor.Redistribe(bucketStatus, s.buckets)
+	if changed {
+		s.buckets = newBuckets
+		s.bucketsLen = len(newBuckets)
+	}
 	return nil
-}
-
-func (*segment) GetWithHash(key string, keyHash uint64) Pair {
-	return nil
-}
-
-func (*segment) Delete(key string) bool {
-	return true
-}
-func (*segment) Size() uint64 {
-	return 0
 }
